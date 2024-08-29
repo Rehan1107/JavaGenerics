767f2a42895f69b906dc30f1b52e9a11dd800a2c
Rehan <pinjarirehan07@gmail.com>
2024-08-29 17:04:16 +0530

    Added: Find Maximum Number Between Three Float Values

--- src/com/bl/java/generics/MaximumFinder.java
+++ src/com/bl/java/generics/MaximumFinder.java
@@ -3,6 +3,7 @@ package com.bl.java.generics;
 public class MaximumFinder {
 
     // Method to find the maximum of three Integer objects
+
     public static Integer findMaximum(Integer x, Integer y, Integer z)
     {
         Integer max = x; // Assume x is the maximum
@@ -19,15 +20,36 @@ public class MaximumFinder {
         return max;
 
     }
+
+    // Method to find the maximum of three float objects
+
+    public static float findMaximum(Float x, Float y, Float z)
+    {
+        float max = x; // Assume x is the maximum
+
+        if (y.compareTo(max) > 0)
+        {
+            max = y; // y is the new maximum
+        }
+        if (z.compareTo(max) > 0)
+        {
+            max = z; // z is the new maximum
+        }
+
+        return max;
+    }
+
     public static void main(String[] args)
     {
         System.out.println("Welcome to Find Maximum Problem using Generics");
 
         // Test cases
         Integer maxInt = findMaximum(9, 5, 7);
+        float maxFloat = findMaximum(8.2f, 10.5f, 9.4f);
 
         // Print Result
         System.out.println("Maximum Number is: " + maxInt);
+        System.out.println("Maximum Number is: " + maxFloat);
 
     }
 }
