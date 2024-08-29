f972e22c03aacfd934812a8010f64df2693486dd
Rehan <pinjarirehan07@gmail.com>
2024-08-29 17:18:20 +0530

    Added: Find Maximum Between Three Strings

--- src/com/bl/java/generics/MaximumFinder.java
+++ src/com/bl/java/generics/MaximumFinder.java
@@ -39,6 +39,24 @@ public class MaximumFinder {
         return max;
     }
 
+    // Method to find the maximum of three String objects
+
+    public static String findMaximum(String x, String y, String z)
+    {
+        String max = x; // Assume x is the Maximum
+
+        if (y.compareTo(max) > 0)
+        {
+            max = y; // y is the new maximum
+        }
+        if (z.compareTo(max) > 0)
+        {
+            max = z; // z is the new maximum
+        }
+
+        return max;
+    }
+
     public static void main(String[] args)
     {
         System.out.println("Welcome to Find Maximum Problem using Generics");
@@ -46,10 +64,12 @@ public class MaximumFinder {
         // Test cases
         Integer maxInt = findMaximum(9, 5, 7);
         float maxFloat = findMaximum(8.2f, 10.5f, 9.4f);
+        String maxString = findMaximum("Apple", "Peach", "Banana");
 
         // Print Result
         System.out.println("Maximum Number is: " + maxInt);
         System.out.println("Maximum Number is: " + maxFloat);
+        System.out.println("Maximum String is: " + maxString);
 
     }
 }
