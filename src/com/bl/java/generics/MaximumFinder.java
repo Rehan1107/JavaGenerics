a03b35993e39b87f5f0552f1c1221d1fa55e9a3f
Rehan <pinjarirehan07@gmail.com>
2024-08-29 17:50:11 +0530

    Added: Refactor All Three Method in One Generic Method

--- src/com/bl/java/generics/MaximumFinder.java
+++ src/com/bl/java/generics/MaximumFinder.java
@@ -2,74 +2,37 @@ package com.bl.java.generics;
 
 public class MaximumFinder {
 
-    // Method to find the maximum of three Integer objects
+    // Generic method to find the maximum of three comparable objects
 
-    public static Integer findMaximum(Integer x, Integer y, Integer z)
+    public static <T extends Comparable<T>> T findMaximum(T x, T y, T z)
     {
-        Integer max = x; // Assume x is the maximum
+            T max = x;  // Assume x is the maximum
 
-        if(y.compareTo(max) > 0)
-        {
-            max = y; // y is the new maximum
-        }
-        if(z.compareTo(max) > 0)
-        {
-            max = z; // z is the new maximum
-        }
+            if (y.compareTo(max) > 0) {
+                max = y;  // y is the new maximum
+            }
 
-        return max;
+            if (z.compareTo(max) > 0) {
+                max = z;  // z is the new maximum
+            }
 
+            return max;
     }
 
-    // Method to find the maximum of three float objects
-
-    public static float findMaximum(Float x, Float y, Float z)
-    {
-        float max = x; // Assume x is the maximum
-
-        if (y.compareTo(max) > 0)
-        {
-            max = y; // y is the new maximum
-        }
-        if (z.compareTo(max) > 0)
-        {
-            max = z; // z is the new maximum
-        }
-
-        return max;
-    }
-
-    // Method to find the maximum of three String objects
-
-    public static String findMaximum(String x, String y, String z)
-    {
-        String max = x; // Assume x is the Maximum
-
-        if (y.compareTo(max) > 0)
-        {
-            max = y; // y is the new maximum
-        }
-        if (z.compareTo(max) > 0)
-        {
-            max = z; // z is the new maximum
-        }
-
-        return max;
-    }
 
     public static void main(String[] args)
     {
         System.out.println("Welcome to Find Maximum Problem using Generics");
 
         // Test cases
-        Integer maxInt = findMaximum(9, 5, 7);
-        float maxFloat = findMaximum(8.2f, 10.5f, 9.4f);
-        String maxString = findMaximum("Apple", "Peach", "Banana");
+        Integer maxInt = findMaximum(9, 5, 7);   // Max Integer Number
+        float maxFloat = findMaximum(8.2f, 10.5f, 9.4f);  // Max float Number
+        String maxString = findMaximum("Apple", "Peach", "Banana");  // Max String
 
-        // Print Result
-        System.out.println("Maximum Number is: " + maxInt);
-        System.out.println("Maximum Number is: " + maxFloat);
-        System.out.println("Maximum String is: " + maxString);
+        // Print results
+        System.out.println("Maximum Number is: " + maxInt);    // Expected: 9
+        System.out.println("Maximum Number is: " + maxFloat);   // Expected: 10.5
+        System.out.println("Maximum String is: " + maxString);    // Expected: Peach
 
     }
 }
