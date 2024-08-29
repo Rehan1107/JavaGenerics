49a329249ec9be3fd4ef3cea0960d056160df904
Rehan <pinjarirehan07@gmail.com>
2024-08-29 15:54:58 +0530

    Added: Welcome Message is added

---
+++ src/com/bl/java/generics/MaximumFinder.java
@@ -0,0 +1,10 @@
+package com.bl.java.generics;
+
+public class MaximumFinder {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Find Maximum Problem using Generics");
+
+    }
+}
