d1b0a53a71f9d9a8c4ae238b54c5361f2fd0f7dd
Rehan <pinjarirehan07@gmail.com>
2024-08-29 16:14:32 +0530

    Added: Find Maximum Number Between Three Integers

--- src/com/bl/java/generics/MaximumFinder.java
+++ src/com/bl/java/generics/MaximumFinder.java
@@ -2,9 +2,32 @@ package com.bl.java.generics;
 
 public class MaximumFinder {
 
+    // Method to find the maximum of three Integer objects
+    public static Integer findMaximum(Integer x, Integer y, Integer z)
+    {
+        Integer max = x; // Assume x is the maximum
+
+        if(y.compareTo(max) > 0)
+        {
+            max = y; // y is the new maximum
+        }
+        if(z.compareTo(max) > 0)
+        {
+            max = z; // z is the new maximum
+        }
+
+        return max;
+
+    }
     public static void main(String[] args)
     {
         System.out.println("Welcome to Find Maximum Problem using Generics");
+
+        // Test cases
+        Integer maxInt = findMaximum(9, 5, 7);
+
+        // Print Result
+        System.out.println("Maximum Number is: " + maxInt);
 
     }
 }
